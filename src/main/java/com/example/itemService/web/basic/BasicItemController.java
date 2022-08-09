d75c4817373a9a0ff604c7cc00f7fd8e1b2c372b
minchoi <53928609+minchoi9509@users.noreply.github.com>
2022-08-09 23:47:28 +0900

    타임리프 진행중

---
+++ src/main/java/com/example/itemService/web/basic/BasicItemController.java
@@ -0,0 +1,33 @@
+package com.example.itemService.web.basic;
+
+import com.example.itemService.domain.item.Item;
+import com.example.itemService.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/item";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10, 10000));
+        itemRepository.save(new Item("itemB", 20, 20000));
+    }
+
+}
